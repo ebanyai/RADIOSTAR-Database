e27983da42c21ee16889a07e5ebfe89351381397
EmiG27 <67435198+EmiG27@users.noreply.github.com>
2020-07-21 13:32:50 +0200

    plot.py update

--- plot.py
+++ plot.py
@@ -1,6 +1,8 @@
 from utils.data_reader import DataModel 
 import utils.data_plotter as dp
 import utils.data_processor as dpr
+import sys, os
+
 
 #NOTE:
 # grab  files' paths from command line ? this would be a really nice addition and 
@@ -10,6 +12,17 @@ import utils.data_processor as dpr
 # -r could be optional
 # (by Evelin)
 
+file_name = input('what file would you like open?')
+with open(file_name)as input:
+    lines = input.readlines()
+    lines_output = ''.join(lines)
+    #print(lines_output)
+    print(file_name)
+
+with open(sys.argv[1], 'r') as file:
+    for line in file:
+        print(line)
+        
 # Getting the data model ready. It includes the species, input_dictionar and 
 # a DataFrame with the imported yield data.
 #data_model = DataModel(r"D:\Projects\Python\RadiostarDB\inputfile_2D_N.txt",r"D:\Projects\Python\RadiostarDB\fruityYields")
@@ -20,3 +33,7 @@ data_to_plot = dpr.process(data_model)
 
 # Create figure(s)
 dp.plot(data_to_plot)
+
+
+    
+    
